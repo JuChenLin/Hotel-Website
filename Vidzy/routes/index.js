a11381ce191c38aa333d8ef9a4bf60547957d6f6
Eric Chou <Eric84605@gmail.com>
2019-11-24 15:31:00 -0600

    passport & method override

--- Vidzy/routes/index.js
+++ Vidzy/routes/index.js
@@ -1,9 +1,207 @@
 var express = require('express');
 var router = express.Router();
+var monk = require('monk');
+var db = monk('localhost:27017/Vidzy');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override');
+
+var passport = require('passport');
+var Account = require('../models/accounts');
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in url - encoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+router.get('/', function(req, res) {
+    res.redirect('/videos');
+});
+
+router.get('/videos', function(req, res) {
+    var collection = db.get('videos');
+    var title = req.query.title;
+    var genre = req.query.genre;
+    var promise = collection.distinct('genre');
+    var username = false;
+    if (req.user) username = req.user.username;
+    console.log(username);
+    promise.then(function(genres){
+        if ((title)  && (genre)) {
+            collection.find({title: {'$regex': title, '$options': 'i'}, genre: genre}, function(err, videos){
+                if (err) throw err;
+  	            //res.json(videos);
+                //console.log(username);
+  	            res.render('index', {title: 'Vidzy', videos: videos, genres: genres, username: username });
+  	            //res.render('index', {title: 'Vidzy', videos: videos, genres: genres, username: username });
+            });
+
+        }
+        else if(title) {
+            collection.find({title: {'$regex': title, '$options': 'i'}}, function(err, videos){
+                if (err) throw err;
+  	            //res.json(videos);
+  	            res.render('index', {title: 'Vidzy', videos: videos, genres: genres, username: username});
+            });
+        }
+        else if(genre) {
+            collection.find({genre: genre}, function(err, videos){
+                if (err) throw err;
+  	            //res.json(videos);
+  	            res.render('index', {title: 'Vidzy', videos: videos, genres: genres, username: username});
+            });
+
+        }
+        else {
+            collection.find({}, function(err, videos){
+                if (err) throw err;
+  	            //res.json(videos);
+  	            res.render('index', {title: 'Vidzy', videos: videos, genres: genres, username: username});
+            });
+        }
+    });
+});
+
+
+router.get('/videos/new', function(req, res) {
+    if (req.user) {
+        if (req.user.role == true)
+            res.render('new');
+    }
+    res.render('noprivilege');
+});
+
+router.get('/videos/:id', function(req, res) {
+    var collection = db.get('videos');
+    collection.findOne({ _id: req.params.id}, function(err, video){
+        if (err) throw err;
+      	//res.json(video);
+      	res.render('show', {video: video})
+    });
+});
+
+router.post('/videos', function(req, res) {
+    if (req.user) {
+        if (req.user.role == true) {
+	        var collection = db.get('videos');
+	        collection.insert({
+		        title: req.body.title,
+		        genre: req.body.genre,
+		        image: req.body.image,
+		        description: req.body.description
+	        }, function(err, video) {
+		        if (err) throw err;
+		        res.redirect('/videos');
+	        });
+        }
+        else {
+            res.render('noprivilege');
+        }
+    }
+    else {
+        res.render('noprivilege');
+    }
+});
+
+router.get('/videos/:id/edit', function(req, res) {
+    if (req.user) {
+        if (req.user.role == true) {
+            var collection = db.get('videos');
+            collection.findOne({ _id: req.params.id}, function(err, video){
+                if (err) throw err;
+                res.render('edit', {video: video})
+            });
+        }
+        else {
+            res.render('noprivilege');
+        }
+    }
+    else {
+        res.render('noprivilege');
+    }
+});
+
+router.put('/videos/:id', function(req, res) {
+    if (req.user) {
+        if (req.user.role == true) {
+            var collection = db.get('videos');
+            collection.findOneAndUpdate({ _id: req.params.id}, {
+                $set: { 
+                    title: req.body.title,
+                    genre: req.body.genre,
+                    image: req.body.image,
+                    description: req.body.description,
+                }
+            }).then((updateDoc) => {});
+            res.redirect('/videos');
+        }
+        else {
+            res.render('noprivilege');
+        }
+    }
+    else {
+        res.render('noprivilege');
+    }
+});
+
+// delete route -for now
+router.delete('/videos/:id', function(req, res) {
+    if (req.user) {
+        if (req.user.role == true) {
+            var collection = db.get('videos');
+            collection.remove({ _id: req.params.id }, function(err, video){
+                if (err) throw err;
+                res.redirect('/');
+            });
+        }
+        else {
+            res.render('noprivilege');
+        }
+    }
+    else {
+        res.render('noprivilege');
+    }
+});
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/register', function(req, res) {
+    res.render('register', { });
+});
+
+router.post('/register', function(req, res) {
+    Account.register(new Account({ username : req.body.username, role : req.body.role }), 
+        req.body.password, function(err, account) {
+        if (err) {
+            return res.render('register', { account : account });
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/');
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { user : req.user });
+    //res.redirect('/');
+});
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
